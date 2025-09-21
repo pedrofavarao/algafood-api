86ddbddbba1aa0aa34dbebfb4dd1e6cd6e488b00
pedrofavarao <pedro.favarao@socin.com.br>
2025-09-21 16:55:24 -0300

    Spring e Injeção de Dependências - Ambiguidade de beans e injeção de listas beans - Desembiguação de beans com @Primary - Desembiguação de beans com @Qualifer coded-by: pedrofavarao

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -1,5 +1,8 @@
 package com.algaworks.algafood.di.notificacao;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
 import com.algaworks.algafood.di.model.Cliente;
 
 import lombok.Getter;
@@ -7,20 +10,20 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@Component
+
+//Permite definir a prioridade da injeção do bean com essa anotação
+//@Primary
+
+@TipoDoIdentificador(NivelUrgencia.SEM_URGENCIA)
 public class NotificadorEmail implements Notificador {
 	
 	private boolean caixaAlta;
-	private String hostServidorSmtp;
 	
-	public NotificadorEmail(String hostServidorSmtp) {
-		this.hostServidorSmtp = hostServidorSmtp;
-		System.out.println(hostServidorSmtp);
-	}
-
 	public void notificar(Cliente cliente, String mensagem) {
 		mensagem = caixaAlta ? mensagem.toUpperCase() : mensagem;
-		System.out.printf("Notificando %s através do e-mail %s usando o host %s: %s\n", 
-				cliente.getNome(), cliente.getEmail(), this.hostServidorSmtp, mensagem);
+		System.out.printf("Notificando %s através do e-mail %s: %s\n", 
+				cliente.getNome(), cliente.getEmail(), mensagem);
 	}
 
 	public boolean isCaixaAlta() {
--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,23 +1,36 @@
 package com.algaworks.algafood.di.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.model.Cliente;
+import com.algaworks.algafood.di.notificacao.NivelUrgencia;
 import com.algaworks.algafood.di.notificacao.Notificador;
+import com.algaworks.algafood.di.notificacao.TipoDoIdentificador;
 
 @Component
 public class AtivacaoClienteService {
 
+	@Autowired(required = false)
+	@TipoDoIdentificador(NivelUrgencia.SEM_URGENCIA)
 	private Notificador notificador;
 	
-	public AtivacaoClienteService(Notificador notificador) {
-		this.notificador = notificador;
-	}
-
+	
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
-
 		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
 	}
 	
+	
+//Resolvendo ambiguidade de varios beans com uma lista
+//	@Autowired(required = false)
+//	private List<Notificador> notificadores;
+//	
+//	
+//	public void ativar(Cliente cliente) {
+//		cliente.ativar();
+//		for (Notificador notificador : notificadores) {
+//			notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+//		}
+//	}
 }
